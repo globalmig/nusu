c108932473429fdc39fae264d47fa11e19e040a8
연구개발전담부 <wdpb@mig.kr>
2025-12-01 11:39:05 +0900

    feat: site map, robot 설정

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.nusudetect.com", // 너 웹사이트 도메인
+  generateRobotsTxt: true, // robots.txt 자동 생성
+  sitemapSize: 7000,
+};
