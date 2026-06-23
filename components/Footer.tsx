93fb10367bd3d449750edbd1ebbf35169ba13594
globalmig <wdpb@mig.kr>
2026-06-23 10:58:42 +0900

    edit: 사업자 주소 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <Link href="/admin/login" className="font-light text-sm">
         관리자페이지
       </Link>
-      <p className="font-light text-sm ">사업자번호 :229-08-02323 ｜ 주소 : 대전광역시 서구 월평서로6번길 49-10, 302호(월평동)</p>
+      <p className="font-light text-sm ">사업자번호 :229-08-02323 ｜ 주소 : 대전광역시 서구 월평서로6번길 49-10</p>
       <p className="font-light text-sm ">전화 : 010-8631-2545 ｜이메일 : op2966@naver.com</p>
     </footer>
   );
